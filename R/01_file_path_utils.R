d960da406facd9c45e024af65fb59057fd17080d
Exceret <15364051195@163.com>
2026-04-21 02:08:01 -0400

    feat: add NEWS.md management utilities

--- R/01_file_path_utils.R
+++ R/01_file_path_utils.R
@@ -1,3 +1,16 @@
 is_pkg <- function(path) {
   dir.exists(path) && file.exists(file.path(path, "DESCRIPTION"))
 }
+
+get_wd <- function() {
+  if (rlang::is_installed("rstudioapi") && interactive()) {
+    current_wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
+  } else {
+    current_wd <- getwd()
+  }
+  if (is_pkg(dirname(current_wd))) {
+    dirname(current_wd)
+  } else {
+    current_wd
+  }
+}
